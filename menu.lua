d248f686fa60c238a7ca668da777328c4225699c
Verty <43362997+W-verty@users.noreply.github.com>
2024-11-20 13:45:18 -0800

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,13 +1,10 @@
-local options = {
-    wall = false,
-    speed = false,
-    aim = false
-};
-
 local Player = game.Players.LocalPlayer;
-local ScreenGui = Instance.new("ScreenGui");
+local ScreenGui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"));
 
 -- Menu
 local menu = Instance.new("vmenu");
 menu.Size = UDim2.new(0.1, 0, 0.25, 0);
+menu.Position = UDim2.new(0.3, 0, 0.2, 0);
+menu.BackgroundColor3 = Color3.fromRGB(50, 50, 50);
+menu.BorderSizePixel = 2;
 menu.Parent = ScreenGui;
